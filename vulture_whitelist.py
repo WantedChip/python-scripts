17f535d442133d1bc3cc9083e73267fc0cda1376
WantedChip <ty121rt@gmail.com>
2026-07-24 23:02:39 +0530

    fix: updated to include the vulture_whitelist globally

--- vulture_whitelist.py
+++ vulture_whitelist.py
@@ -1,21 +1,28 @@
-threshold_date
-homepage
-user_found
-has_write
-has_read
-remediation
-lang
-load_mock
-other_file
-parse_qsl
-urlunparse
-dflt
-compared_pairs
-full_adjacent
-do_OPTIONS
-do_HEAD
-tesseract_cmd
-victim_test
-culprit_tests
-seed_used
-reproduce_command
+"""Vulture whitelist file for unused code detection across repository tools."""
+
+# pylint: disable=pointless-statement
+from typing import Any
+
+_: Any = None
+
+_.threshold_date
+_.homepage
+_.user_found
+_.has_write
+_.has_read
+_.remediation
+_.lang
+_.load_mock
+_.other_file
+_.parse_qsl
+_.urlunparse
+_.dflt
+_.compared_pairs
+_.full_adjacent
+_.do_OPTIONS
+_.do_HEAD
+_.tesseract_cmd
+_.victim_test
+_.culprit_tests
+_.seed_used
+_.reproduce_command
